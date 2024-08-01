1fc987eab30c33545f7a7d6d278fdee1c46d99d5
nataliamd11 <nataliamdenk11@gmail.com>
2024-08-01 18:23:56 -0300

    chore: install and configure django cors headers

--- demo/django_vue_demo/settings.py
+++ demo/django_vue_demo/settings.py
@@ -29,7 +29,10 @@ DJANGO_VITE_DEV_MODE = DEBUG
 # Using the same port as the dev port defined in vite.config.js
 DJANGO_VITE_DEV_SERVER_PORT = 3000
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
@@ -42,9 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_vite',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware", # add this line
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
