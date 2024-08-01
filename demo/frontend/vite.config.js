a701166b3281e2371195e238773e82ec2bde79fe
nataliamd11 <nataliamdenk11@gmail.com>
2024-08-01 18:29:59 -0300

    chore: edit vite configuration

--- demo/frontend/vite.config.js
+++ demo/frontend/vite.config.js
@@ -1,16 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import path from "node:path"; // add this import
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+// define this variables
+const INPUT_DIR = "./src";
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+ plugins: [
+   vue(),
+ ],
+ resolve: { // change the resolve.alias
+   alias: {
+     "@": path.resolve(INPUT_DIR),
+   },
+ },
+ root: path.resolve(INPUT_DIR),
+ base: "/static/",
+ server: {
+   origin: "http://localhost:3000",
+   host: "localhost",
+   port: 3000,
+ },
 })
