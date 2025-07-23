394489efc68c591146d1ae48b9f811691f35884e
nataliamd11 <nataliamdenk11@gmail.com>
2025-07-23 12:28:52 -0300

    feat: add dog model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,19 @@
+# DjangoVueConnection/backend/models.py
+
+""" Models for the backend app """
+
 from django.db import models
 
-# Create your models here.
+
+class Dog(models.Model):
+   """
+   Model for a dog
+   """
+   name = models.CharField(max_length=255)
+   age = models.IntegerField()
+   color = models.CharField(max_length=255)
+   size = models.CharField(max_length=50)
+
+
+   def __str__(self):
+       return self.name
