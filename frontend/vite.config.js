817b4aebdb89816c82f6b81b2a993f9b0eb7138d
nataliamd11 <nataliamdenk11@gmail.com>
2025-07-23 07:08:13 -0300

    feat: add build and output dir variable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,39 @@
 // DjangoVueConnection/frontend/vite.config.js
 
-import path from "node:path"; // add this import
-
-
+import path from "node:path";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 
-// define this variables
 const INPUT_DIR = "./src";
+// define this variable
+const OUTPUT_DIR = "./static/dist";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [
-   vue(),
- ],
- resolve: { // change the resolve.alias
-   alias: {
-     "@": path.resolve(INPUT_DIR),
-   },
- },
- root: path.resolve(INPUT_DIR),
- base: "/static/",
- server: {
-   origin: "http://localhost:3000",
-   host: "localhost",
-   port: 3000,
- },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(INPUT_DIR),
+    },
+  },
+  root: path.resolve(INPUT_DIR),
+  base: "/static/",
+  server: {
+    origin: "http://localhost:3000",
+    host: "localhost",
+    port: 3000,
+  },
+  build: { // add build dictionary
+    manifest: true,
+    outDir: path.resolve(OUTPUT_DIR),
+    rollupOptions: {
+      input: {
+        main: path.join(INPUT_DIR, "/main.js"),
+      },
+    },
+  },
 })
