1a7a6b03630841c134d064975e3c7c8a22f72625
nataliamd11 <nataliamdenk11@gmail.com>
2025-07-23 13:14:56 -0300

    feat: add view class to get dogs

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# DjangoVueConnection/backend/views.py
 
-# Create your views here.
+""" Views for the backend app """
+
+from django.views import View
+from django.http import JsonResponse
+from backend.models import Dog
+
+
+class DogsView(View):
+   """
+   API endpoint for dogs data
+   """
+   def get(self, request):
+       """
+       Get all dogs data
+       """
+       dogs = Dog.objects.all().values()
+       return JsonResponse(
+           {'results': list(dogs)},
+           status=200
+       )
