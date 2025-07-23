b7525814dc73341490a51b04e187085c9f725b76
nataliamd11 <nataliamdenk11@gmail.com>
2025-07-23 18:07:34 -0300

    feat: create DogsData vue component

---
+++ frontend/src/components/DogsData.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { getDogs } from "../api_connection/dogsRequest";
+
+const dogs = ref([]);
+
+onMounted(async () => {
+   dogs.value = await getDogs();
+});
+</script>
+
+
+<template>
+   <div>
+       <h1>Dogs</h1>
+       <div v-for="dog in dogs" :key="dog.id">
+           <div class="dog-data">
+               <h2>{{ dog.name }}</h2>
+               <ul>
+                   <li>
+                       Age: {{ dog.age }}
+                   </li>
+                   <li>
+                       Color: {{ dog.color }}
+                   </li>
+                   <li>
+                       Size: {{ dog.size }}
+                   </li>
+               </ul>
+           </div>
+       </div>
+   </div>
+</template>
+
+
+<style scoped>
+.dog-data {
+   width: 250px;
+   border: 1px solid #ccc;
+   padding: 10px;
+   margin: 10px;
+   border-radius: 5px;
+   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+}
+</style>
